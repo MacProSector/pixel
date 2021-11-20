0fd7e82d56e33a87f56e69fe08f1568611088224
Simon Yu <jundayu2@illinois.edu>
2021-11-20 00:29:55 -0600

    Update task init messages

--- kano_pixel_kit_firmware.ino
+++ kano_pixel_kit_firmware.ino
@@ -15,19 +15,22 @@ using kano_pixel_kit::NeoPixel;
 std::shared_ptr<Display> display_;
 std::shared_ptr<Logger> logger_;
 
+const std::string task_name_core_0("Core 0");
+const std::string task_name_core_1("Core 1");
+
 void
 taskCore0(void *pvParameters)
 {
-    logger_->logInfo("Started core 0 task");
+    logger_->logInfo("Started task \"" + task_name_core_0 + "\" on core " + std::to_string(xPortGetCoreID()));
 
-    std::vector<Eigen::Vector3i> frame;
+    auto frame = std::make_shared<std::vector<Eigen::Vector3i>>();
 
     for (int i = 0; i < static_cast<int>(NeoPixel::size); i ++)
     {
-        frame.push_back(Eigen::Vector3i(10, 10, 10));
+        frame->push_back(Eigen::Vector3i(10, 10, 10));
     }
 
-    display_->setFrame(frame);
+    display_->setFrame(*frame);
 
     for(;;)
     {
@@ -38,7 +41,7 @@ taskCore0(void *pvParameters)
 void
 taskCore1(void *pvParameters)
 {
-    logger_->logInfo("Started core 1 task");
+    logger_->logInfo("Started task \"" + task_name_core_1 + "\" on core " + std::to_string(xPortGetCoreID()));
 
     for(;;)
     {
@@ -55,8 +58,8 @@ setup()
     logger_->initialize();
     display_->initialize(logger_);
 
-    xTaskCreatePinnedToCore(taskCore0, "Core 0 Task", 1024, NULL, 3, NULL, 0);
-    xTaskCreatePinnedToCore(taskCore1, "Core 1 Task", 1024, NULL, 3, NULL, 1);
+    xTaskCreatePinnedToCore(taskCore0, task_name_core_0.c_str(), 2048, NULL, 3, NULL, 0);
+    xTaskCreatePinnedToCore(taskCore1, task_name_core_1.c_str(), 2048, NULL, 3, NULL, 1);
 }
 
 void
